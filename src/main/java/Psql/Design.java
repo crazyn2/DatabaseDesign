7e62db7c2a9114e6392e3df45e5dba672d3918f6
crazyn2 <13868637093@163.com>
2020-07-13 18:31:05 +0800

    图形界面

--- src/main/java/Psql/Design.java
+++ src/main/java/Psql/Design.java
@@ -1,7 +1,6 @@
 package Psql;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.*;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -12,10 +11,8 @@ import javax.swing.JTextArea;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
-import java.awt.FlowLayout;
-import java.awt.Frame;
 
-public class Design extends JFrame {
+public class Design extends JFrame {//图形界面
 
 	/**
 	 * 
@@ -27,16 +24,16 @@ public class Design extends JFrame {
 	protected JTextField textField_1 = new JTextField();
 	protected JTextField textField_2 = new JTextField();
 	protected JPanel panel = new JPanel();
-	protected JButton btnNewButton = new JButton("count-by-year");
-	protected JButton btnNewButton_1 = new JButton("order-by-score");
-	protected JButton btnNewButton_2 = new JButton("count-by-avg");
-	protected JButton btnNewButton_4 = new JButton("count-by-courses");
-	protected JButton btnNewButton_3 = new JButton("query students");
-	protected JLabel lblNewLabel = new JLabel("student number");
-	protected JButton btnNewButton_5 = new JButton("query students");
-	protected JLabel lblNewLabel_1 = new JLabel("teacher number");
-	protected JLabel lblNewLabel_2 = new JLabel("class number");
-	protected JButton btnNewButton_7 = new JButton("courses of class query");
+	protected JButton btnNewButton = new JButton("学生成绩按每学年成绩统计");//("count-by-year");
+	protected JButton btnNewButton_1 = new JButton("学生成绩名次排定");
+	protected JButton btnNewButton_2 = new JButton("每门课程平均成绩统计");
+	protected JButton btnNewButton_3 = new JButton("学生所学课程及学分统计");
+	protected JButton btnNewButton_4 = new JButton("学生成绩查询");
+	protected JLabel lblNewLabel = new JLabel("查询的学号:");
+	protected JButton btnNewButton_5 = new JButton("教师任课查询");
+	protected JLabel lblNewLabel_1 = new JLabel("查询的教师号:");
+	protected JLabel lblNewLabel_2 = new JLabel("查询的班级号:");
+	protected JButton btnNewButton_7 = new JButton("班级课程开设查询");
 	protected JScrollPane scrollPane = new JScrollPane();
 	protected JTextArea textArea = new JTextArea();
 	/**
@@ -45,14 +42,16 @@ public class Design extends JFrame {
 	public Design() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 910, 503);
-
+		textArea.setEditable(false);
+		textArea.setBackground(Color.BLACK);
+		textArea.setForeground(Color.green);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
 		
-		panel.setPreferredSize(new Dimension(0, 150));
+		panel.setPreferredSize(new Dimension(0, 120));
 		contentPane.add(panel, BorderLayout.NORTH);
 		panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		
@@ -61,30 +60,29 @@ public class Design extends JFrame {
 		panel.add(btnNewButton_1);
 		
 		panel.add(btnNewButton_2);
-		
+
 		panel.add(btnNewButton_3);
+
 		
 		panel.add(lblNewLabel);
 		
 		panel.add(textField);
 		textField.setColumns(10);
-		
-		panel.add(btnNewButton_5);
-		
-		panel.add(lblNewLabel_1);
-		
-		panel.add(textField_1);
-		textField_1.setColumns(10);
-		
+
 		panel.add(btnNewButton_4);
 		
+		panel.add(lblNewLabel_1);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		panel.add(btnNewButton_5);
+
 		panel.add(lblNewLabel_2);
 		
 		textField_2.setText("");
 		panel.add(textField_2);
 		textField_2.setColumns(10);
-		
 		panel.add(btnNewButton_7);
+
 		
 		contentPane.add(scrollPane, BorderLayout.CENTER);
 		
--- src/main/java/Psql/Psqlbase.java
+++ src/main/java/Psql/Psqlbase.java
@@ -1,4 +1,5 @@
 package Psql;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -10,32 +11,6 @@ public class Psqlbase extends Design{
 	static Connection connect = null;
 	Statement stmt = null;
 	String[] sql = new String[8];
-//			{"select zby_term,zby_sno,sum(zby_score) as zby_sum from zhangbingyang_reports "
-//			+ "group by zby_term,zby_sno",
-//			"select *,rank() over( order by zby_score desc) from zhangbingyang_reports",
-//			"select zby_cno,avg(zby_score) as zby_avg from zhangbingyang_reports group by zby_cno",
-//			"select zby_sno,sum(zby_value) as zby_sum from zhangbingyang_reports as zbyr"
-//					+ " join zhangbingyang_courses as zbyc on zbyr.zby_cno=zbyc.zby_cno "
-//					+ "group by zbyr.zby_sno",
-//			"select zby_sno,zbyr.zby_cno,zby_name from  zhangbingyang_reports " +
-//					"as zbyr join zhangbingyang_courses as zbyc on zbyr.zby_cno=zbyc.zby_cno",
-//			"select zby_score,zby_cno from zhangbingyang_reports where zby_sno =",
-//			"select * from zhangbingyang_courses where zby_tno =",
-//			"select * from zhangbingyang_courses where zby_csno ="
-//	};
-	String csno = "R01";
-//	sql[1] = String("select *,rank() over( order by zby_score desc) from zhangbingyang_reports");
-//	sql[0] = "select zby_term,zby_sno,sum(zby_score) as zby_sum from zhangbingyang_reports "
-//			+ "group by zby_term,zby_sno";
-//	sql[2] = "select zby_cno,avg(zby_score) as zby_avg from zhangbingyang_reports group by zby_cno";
-//	sql[3] = "select zby_sno,sum(zby_value) as zby_sum from zhangbingyang_reports as zbyr"
-//			+ " join zhangbingyang_courses as zbyc on zbyr.zby_cno=zbyc.zby_cno "
-//			+ "group by zbyr.zby_sno";
-//	sql[4] = "select zby_sno,zbyr.zby_cno,zby_name from  zhangbingyang_reports " +
-//			"as zbyr join zhangbingyang_courses as zbyc on zbyr.zby_cno=zbyc.zby_cno";
-//	sql[5] = "select zby_score,zby_cno from zhangbingyang_reports where zby_sno =";
-//	sql[6] = "select * from zhangbingyang_courses where zby_tno =";
-//	sql[7] = "select * from zhangbingyang_courses where zby_csno =";
 	public Psqlbase(){
 		sql[0] = "select zby_term,zby_sno,sum(zby_score) as zby_sum from zhangbingyang_reports "
 				+ "group by zby_term,zby_sno";
@@ -61,7 +36,9 @@ public class Psqlbase extends Design{
 		btnNewButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				try{
+				Thread0 thread0 = new Thread0();
+				thread0.start();
+				/*try{
 					ResultSet rs = stmt.executeQuery(sql[0]);
 					Psql0 psql0 = new Psql0();
 					textArea.setText("");
@@ -76,7 +53,7 @@ public class Psqlbase extends Design{
 				}catch (Exception e1) {
 					System.err.print("get data error!");
 					e1.printStackTrace();
-				}
+				}*/
 			}
 		});
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -126,9 +103,154 @@ public class Psqlbase extends Design{
 		btnNewButton_3.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
+				try{
+					ResultSet rs = stmt.executeQuery(sql[3]);
+					Psql3 psql3 = new Psql3();
+					textArea.setText("");
+					if(!rs.isBeforeFirst()){
+						textArea.setText("None\n");
+					}
+					else {
+						textArea.append("student's total counts\n");
+						while (rs.next()) {
+							psql3.setSno(rs.getString("zby_sno"));
+							psql3.setSum(rs.getInt("zby_sum"));
+							String json = new Gson().toJson(psql3);
+							textArea.append(json + "\n");
+						}
+						rs = stmt.executeQuery(sql[4]);
+						Psql4 psql4 = new Psql4();
+						textArea.append("student's courses\n");
+						while (rs.next()) {
+							psql4.setSno(rs.getString("zby_sno"));
+							psql4.setCno(rs.getString("zby_cno"));
+							psql4.setName(rs.getString("zby_name"));
+							String json = new Gson().toJson(psql4);
+							textArea.append(json + "\n");
+						}
+					}
+					rs.close();
+				}catch (Exception e4){
+					System.err.print("get data error!");
+					System.exit(-1);
+					e4.printStackTrace();
+				}
 			}
 		});
+		btnNewButton_4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = textField.getText();
+				try{
+					String tmp = sql[5] + "'" + num + "'";
+					ResultSet rs = stmt.executeQuery(tmp);
+					Psql5 psql5 = new Psql5();
+					textArea.setText("");
+					if(!rs.isBeforeFirst()){
+						textArea.append("None\n");
+					}
+					else {
+						while (rs.next()) {
+							psql5.setSno(num);
+							psql5.setCno(rs.getString("zby_cno"));
+							psql5.setScore(rs.getInt("zby_score"));
+							String json = new Gson().toJson(psql5);
+							textArea.append(json + "\n");
+						}
+					}
+					rs.close();
+				}catch (Exception e5){
+					System.err.print("get data error!");
+					e5.printStackTrace();
+					System.exit(-1);
+				}
+			}
+		});
+		btnNewButton_5.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try{
+					String num = textField_1.getText();
+					String tmp = sql[6] + "'" + num + "'";
+					ResultSet rs = stmt.executeQuery(tmp);
+					textArea.setText("");
+					if(!rs.isBeforeFirst()){
+						textArea.setText("None\n");
+					}
+					else{
+						Courses courses = new Courses();
+						while (rs.next()) {
+							courses.setName(rs.getString("zby_name"));
+							courses.setTerm(rs.getString("zby_term"));
+							courses.setCno(rs.getString("zby_cno"));
+							courses.setTime(rs.getString("zby_time"));
+							courses.setTno(rs.getString("zby_tno"));
+							courses.setExam(rs.getString("zby_exam"));
+							courses.setValue(rs.getString("zby_value"));
+							String json = new Gson().toJson(courses);
+							textArea.append(json + "\n");
+						}
+					}
+					rs.close();
+				}catch (Exception e5){
+					System.err.print("get data error!");
+					e5.printStackTrace();
+					System.exit(-1);
+				}
+			}
+		});
+		btnNewButton_7.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try{
+					String num = textField_2.getText();
+					ResultSet rs = stmt.executeQuery(sql[7] + "'" + num + "'");
+					textArea.setText("");
+					if(!rs.isBeforeFirst()){
+						textArea.append("None");
+					}
+					else{
+						Courses courses = new Courses();
+						while (rs.next()) {
+							courses.setName(rs.getString("zby_name"));
+							courses.setTerm(rs.getString("zby_term"));
+							courses.setCno(rs.getString("zby_cno"));
+							courses.setTime(rs.getString("zby_time"));
+							courses.setTno(rs.getString("zby_tno"));
+							courses.setExam(rs.getString("zby_exam"));
+							courses.setValue(rs.getString("zby_value"));
+							String json = new Gson().toJson(courses);
+							textArea.append(json + "\n");
+						}
+					}
+					rs.close();
+				}catch (Exception e6){
+					System.err.print("get data error!");
+					e6.printStackTrace();
+					System.exit(-1);
+				}
+			}
+		});
+	}
+	private class Thread0 extends Thread{
+		public void run(){
+			try{
+				ResultSet rs = stmt.executeQuery(sql[0]);
+				Psql0 psql0 = new Psql0();
+				textArea.setText("");
+				while (rs.next()) {
+					psql0.setSno(rs.getString("zby_sno"));
+					psql0.setTerm(rs.getString("zby_term"));
+					psql0.setSum(rs.getInt("zby_sum"));
+					String json = new Gson().toJson(psql0);
+					textArea.append(json + "\n");
+				}
+				rs.close();
+			}catch (Exception e1) {
+				System.err.print("get data error!");
+				e1.printStackTrace();
+			}
+		}
 	}
 	public void countByYear(){
 		try{
@@ -184,7 +306,116 @@ public class Psqlbase extends Design{
 		}
 	}
 	public void countByCourses(){
-		
+		try{
+			ResultSet rs = stmt.executeQuery(sql[3]);
+			Psql3 psql3 = new Psql3();
+			if(!rs.isBeforeFirst()){
+				System.out.println("查无课程统计学分");
+				System.exit(0);
+			}
+			System.out.println("student course score count");
+			while (rs.next()) {
+				psql3.setSno(rs.getString("zby_sno"));
+				psql3.setSum(rs.getInt("zby_sum"));
+				String json = new Gson().toJson(psql3);
+				System.out.println(json);
+			}
+			rs = stmt.executeQuery(sql[4]);
+			Psql4 psql4 = new Psql4();
+			System.out.println("student's courses count");
+			while (rs.next()) {
+				psql4.setSno(rs.getString("zby_sno"));
+				psql4.setCno(rs.getString("zby_cno"));
+				psql4.setName(rs.getString("zby_name"));
+				String json = new Gson().toJson(psql4);
+				System.out.println(json);
+			}
+			rs.close();
+		}catch (Exception e){
+			System.err.print("get data error!");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	public void queryStudent(String num){
+		try{
+			String tmp = sql[5] + "'" + num + "'";
+			ResultSet rs = stmt.executeQuery(tmp);
+			Psql5 psql5 = new Psql5();
+			if(!rs.isBeforeFirst()){
+				System.out.println("查无此人");
+				System.exit(-1);
+			}
+			while (rs.next()) {
+				psql5.setSno(num);
+				psql5.setCno(rs.getString("zby_cno"));
+				psql5.setScore(rs.getInt("zby_score"));
+				String json = new Gson().toJson(psql5);
+				System.out.println(json);
+			}
+			rs.close();
+		}catch (Exception e){
+			System.err.print("get data error!");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	public void queryTeacher(String num){
+		try{
+			String tmp = sql[6] + "'" + num + "'";
+			ResultSet rs = stmt.executeQuery(tmp);
+			if(!rs.isBeforeFirst()){
+				System.err.print("null");
+				System.exit(-1);
+			}
+			else{
+				Courses courses = new Courses();
+				while (rs.next()) {
+					courses.setName(rs.getString("zby_name"));
+					courses.setTerm(rs.getString("zby_term"));
+					courses.setCno(rs.getString("zby_cno"));
+					courses.setTime(rs.getString("zby_time"));
+					courses.setTno(rs.getString("zby_tno"));
+					courses.setExam(rs.getString("zby_exam"));
+					courses.setValue(rs.getString("zby_value"));
+					String json = new Gson().toJson(courses);
+					System.out.println(json);
+				}
+			}
+			rs.close();
+		}catch (Exception e){
+			System.err.print("get data error!");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+	public void queryClass(String num){
+		try{
+			ResultSet rs = stmt.executeQuery(sql[7] + "'" + num + "'");
+			if(!rs.isBeforeFirst()){
+				System.out.println("查无此班");
+				System.exit(0);
+			}
+			else{
+				Courses courses = new Courses();
+				while (rs.next()) {
+					courses.setName(rs.getString("zby_name"));
+					courses.setTerm(rs.getString("zby_term"));
+					courses.setCno(rs.getString("zby_cno"));
+					courses.setTime(rs.getString("zby_time"));
+					courses.setTno(rs.getString("zby_tno"));
+					courses.setExam(rs.getString("zby_exam"));
+					courses.setValue(rs.getString("zby_value"));
+					String json = new Gson().toJson(courses);
+					System.out.println(json);
+				}
+			}
+			rs.close();
+		}catch (Exception e){
+			System.err.print("get data error!");
+			e.printStackTrace();
+			System.exit(-1);
+		}
 	}
 	public static void main(String args[]) {
 		try {
@@ -192,7 +423,7 @@ public class Psqlbase extends Design{
 
 //			Statement stmt = connect.createStatement();
 			Psqlbase psqlbase = new Psqlbase();
-			if(args.length == 0) {
+			if(args.length == 0) {//没有参数启动图形界面
 				psqlbase.setVisible(true);
 				System.out.println("Usage:\n{--count-by-year " +
 						"| --order-by-score | --count-by-avg " +
@@ -201,143 +432,34 @@ public class Psqlbase extends Design{
 				return;
 			}
 			switch(args[0]){
-				case "--count-by-year": {
+				case "--count-by-year": {//学生成绩按每学年成绩统计
 					psqlbase.countByYear();
-//					ResultSet rs = stmt.executeQuery(sql[0]);
-//					Psql0 psql0 = new Psql0();
-//					while (rs.next()) {
-//						psql0.setSno(rs.getString("zby_sno"));
-//						psql0.setTerm(rs.getString("zby_term"));
-//						psql0.setSum(rs.getInt("zby_sum"));
-//						String json = new Gson().toJson(psql0);
-//						System.out.println(json);
-//					}
-//					rs.close();
 				};
 				break;
-				case "--order-by-score":{
+				case "--order-by-score":{//学生成绩名次排定
 					psqlbase.orderByScore();
-//					ResultSet rs = stmt.executeQuery(sql[1]);
-//					Srank srank = new Srank();
-//					while (rs.next()) {
-//						srank.setSno(rs.getString("zby_sno"));
-//						srank.setTerm(rs.getString("zby_term"));
-//						srank.setCno(rs.getString("zby_cno"));
-//						srank.setScore(rs.getInt("zby_score"));
-//						srank.setTno(rs.getString("zby_tno"));
-//						srank.setRank(rs.getInt("rank"));
-//						String json = new Gson().toJson(srank);
-//						System.out.println(json);
-//					}
-//					rs.close();
 				};
 				break;
-				case "--count-by-avg":{
+				case "--count-by-avg":{//每门课程平均成绩统计
 					psqlbase.countByYear();
-//					ResultSet rs = stmt.executeQuery(sql[2]);
-//					Psql2 psql2 = new Psql2();
-//					while (rs.next()) {
-//						psql2.setCno(rs.getString("zby_cno"));
-//						psql2.setAvg(rs.getDouble("zby_avg"));
-//						String json = new Gson().toJson(psql2);
-//						System.out.println(json);
-//					}
-//					rs.close();
 				}
 				break;
-				case "--count-by-courses":{
-					ResultSet rs = stmt.executeQuery(sql[3]);
-					Psql3 psql3 = new Psql3();
-					if(!rs.isBeforeFirst()){
-						System.out.println("查无课程统计学分");
-						break;
-					}
-					System.out.println("student course score count");
-					while (rs.next()) {
-						psql3.setSno(rs.getString("zby_sno"));
-						psql3.setSum(rs.getInt("zby_sum"));
-						String json = new Gson().toJson(psql3);
-						System.out.println(json);
-					}
-					rs = stmt.executeQuery(sql[4]);
-					Psql4 psql4 = new Psql4();
-					if(!rs.isBeforeFirst()){
-						System.out.println("查无");
-						break;
-					}
-					System.out.println("student's courses count");
-					while (rs.next()) {
-						psql4.setSno(rs.getString("zby_sno"));
-						psql4.setCno(rs.getString("zby_cno"));
-						psql4.setName(rs.getString("zby_name"));
-						String json = new Gson().toJson(psql4);
-						System.out.println(json);
-					}
-					rs.close();
+				case "--count-by-courses":{//学生所学课程及学分统计
+					psqlbase.countByCourses();
 				}
 				break;
-				/*case "-qs":{
-					sql[5] = sql[5] + "'" + args[1] + "'";
-					ResultSet rs = stmt.executeQuery(sql[5]);
-					Psql5 psql5 = new Psql5();
-					if(!rs.isBeforeFirst()){
-						System.out.println("查无此人");
-						break;
-					}
-					while (rs.next()) {
-						psql5.setSno(args[1]);
-						psql5.setCno(rs.getString("zby_cno"));
-						psql5.setScore(rs.getInt("zby_score"));
-						String json = new Gson().toJson(psql5);
-						System.out.println(json);
-					}
-					rs.close();
+				case "-qs":{//学生成绩查询
+					psqlbase.queryStudent(args[1]);
 				}
 				break;
-				case "-qt":{
-					sql[6] = sql[6] + "'" + args[1] + "'";
-					ResultSet rs = stmt.executeQuery(sql[6]);
-					if(!rs.isBeforeFirst()){
-						System.out.println("查无此人");
-						break;
-					}
-					Courses courses = new Courses();
-					while (rs.next()) {
-						courses.setName(rs.getString("zby_name"));
-						courses.setTerm(rs.getString("zby_term"));
-						courses.setCno(rs.getString("zby_cno"));
-						courses.setTime(rs.getString("zby_time"));
-						courses.setTno(rs.getString("zby_tno"));
-						courses.setExam(rs.getString("zby_exam"));
-						courses.setValue(rs.getString("zby_value"));
-						String json = new Gson().toJson(courses);
-						System.out.println(json);
-					}
-					rs.close();
+				case "-qt":{//教师任课查询
+					psqlbase.queryTeacher(args[1]);
 				}
 				break;
-				case "-qr":{
-					sql[7] = sql[7] + "'" + args[1] + "'" ;
-					ResultSet rs = stmt.executeQuery(sql[7]);
-					if(!rs.isBeforeFirst()){
-						System.out.println("查无此班");
-						break;
-					}
-					Courses courses = new Courses();
-					while (rs.next()) {
-						courses.setName(rs.getString("zby_name"));
-						courses.setTerm(rs.getString("zby_term"));
-						courses.setCno(rs.getString("zby_cno"));
-						courses.setTime(rs.getString("zby_time"));
-						courses.setTno(rs.getString("zby_tno"));
-						courses.setExam(rs.getString("zby_exam"));
-						courses.setValue(rs.getString("zby_value"));
-						String json = new Gson().toJson(courses);
-						System.out.println(json);
-					}
-					rs.close();
+				case "-qr":{//班级课程开设查询
+					psqlbase.queryClass(args[1]);
 				}
-				break;*/
+				break;
 				default:
 					System.out.println("Usage:\n{--count-by-year " +
 							"| --order-by-score | --count-by-avg " +
@@ -345,13 +467,10 @@ public class Psqlbase extends Design{
 							"| -qr <classroom number> }");
 			}
 			System.out.println("Complete");
-//			stmt.close();
-//			connect.close();
 		} catch (Exception e) {
 			System.err.print("get data error!");
 			e.printStackTrace();
 		}
-//		guiClass.setVisible(true);
 	}
 	protected void destructor(){
 		try {
